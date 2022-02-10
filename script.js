cb33502b4ca2de4e89a7787f19e4a2449507a51a
Muhammad Iqbal <muhammadiqbalid63@gmail.com>
2022-02-10 10:19:54 +0700

    url string

--- script.js
+++ script.js
@@ -48,17 +48,29 @@
 })();
 
 // Initialize and add the map
-      function initMap() {
-        // The location of Uluru
-        const uluru = { lat: -25.344, lng: 131.036 };
-        // The map, centered at Uluru
-        const map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru,
-        });
-        // The marker, positioned at Uluru
-        const marker = new google.maps.Marker({
-          position: uluru,
-          map: map,
-        });
-      }
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+//get parameters javascript
+function getParameter(parameterName) {
+  let parameters = new URLSearchParams(window.location.search);
+  return parameters.get(parameterName);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const name = getParameter('name');
+
+  document.getElementById('myTitle').innerHTML = name;
+});
